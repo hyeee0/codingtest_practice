c922ca9ebbed3bacfd34e72449644b17e1aa8a91
hyeee0 <118868938+hyeee0@users.noreply.github.com>
2023-06-15 20:41:19 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    answer = 0
+    for i in range(len(num_list)):
+        if num_list[i] < 0:
+            print(i)
+            answer = i
+            break
+        elif num_list[i] > 0:
+            answer = -1
+    return answer
